06365fad94b298067cc51d44d2c5d5d73b22cd80
mapster <mapster@ndt-lan.no>
2013-02-06 13:58:48 +0100

    Added a demo java source

---
+++ web/StaticTest.java
@@ -0,0 +1,37 @@
+class StaticTest {
+    static String s = "hei";
+    static String s2 = s + "sann";
+    static int tall0 = 3-1;
+    static int mult = 3*tall0;
+    static int div = 6/2;
+    static int tall2 = 4 + 3 + 2 + 1;
+    static int tall3 = funksjon(fem());
+    static int tall4 = funksjon3(fem(), funksjon2(fem()), 3);
+    static double d1 = 2.0 * tall0;
+
+    static int farray(String[] liste){
+        return 1;
+    }
+
+    static int funksjon(int tall){
+        tall0 = tall;
+        return funksjon2(9);
+    }
+
+    static int funksjon2(int tall){
+        tall2 = 1;
+        if(true)
+            return tall;
+        else
+            return 1;
+    }
+
+    static int fem(){
+        return 5;
+    }
+
+    static int funksjon3(int tall, int tall2, int tall3){
+        return tall2;
+    }
+}
+
