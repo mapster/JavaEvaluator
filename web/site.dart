bddc8010c6707b76badd2cf9ab0767891678f6f0
Anya Helene Bagge <anya@ii.uib.no>
2013-07-09 22:49:17 +0200

    Change UI a bit; add example selection

--- web/site.dart
+++ web/site.dart
@@ -10,6 +10,7 @@ part '../printer.dart';
 
 String exampleSource = "class StaticTest { static int tall0; static int tall2 = 4; static int tall3 = funksjon(fem()); static int tall4 = funksjon3(fem(), funksjon2(fem()), 3); static int funksjon(int tall){ tall0 = tall; return funksjon2(9); }static int funksjon2(int tall){ tall2 = 1; return tall; } static int fem(){ return 5; } static int funksjon3(int tall, int tall2, int tall3){ return tall2;}}";
 String toJsonUrl = "/tojson";
+String javaUrl = "/java";
 
 DivElement value = query("#lastvalue");
 DivElement java = query("#java");
@@ -18,17 +19,55 @@ TableElement memory = query("#memory");
 Program prog;
 Runner runner;
 InputElement stepBtn = query("#step");
-
+Element selectSrcCode = query("#selectSourceCode");
+InputElement selectBtn = query("#select");
 void main() {
   InputElement bruk = query("#bruk");
 
   //  Parser.prog.root.map(f)
   bruk.onClick.listen((Event e){readFile();});
   stepBtn.onClick.listen((Event e){step();});
-  
-  query("#example").onClick.listen((Event e){postSourceToJsonService(name:"StaticTest.java", source:exampleSource);});
+  selectSrcCode.onClick.listen((Event e) {toggleSelectSrcCode();});
+  selectBtn.onClick.listen((Event e){selectClicked(e);});
   
 //  drawArrow(new Pos(), new Pos(), 7);
+}
+
+void selectClicked(Event e) {
+  switch(selectBtn.value) {
+    case "*upload*":
+      query("#uploadFile").style.display = "block";
+      query("#inputCode").style.display = "none";
+      selectBtn.size = 3;
+      break;
+    case "*input*":
+      query("#uploadFile").style.display = "none";
+      query("#inputCode").style.display = "block";
+      selectBtn.size = 3;
+      break;
+    case "":
+      query("#uploadFile").style.display = "none";
+      query("#inputCode").style.display = "none";
+      selectBtn.size = selectBtn.children.length;
+      break;
+    default:
+      query("#uploadFile").style.display = "none";
+      query("#inputCode").style.display = "none";
+      selectBtn.size = selectBtn.children.length;
+      String fileName = selectBtn.value;
+      if(new RegExp(r"[a-zA-Z]+\.java$").matchAsPrefix(fileName) != null) {
+        HttpRequest.getString(javaUrl + "/" + fileName)
+          .then((result) { postSourceToJsonService(name:selectBtn.value, source:result); print(result); });
+      }
+      break;
+  }
+}
+
+void toggleSelectSrcCode() {
+  if(selectSrcCode.style.backgroundColor == "rgb(255, 0, 0)")
+    selectSrcCode.style.backgroundColor = "#fff0f0";
+  else
+    selectSrcCode.style.backgroundColor = "#ff0000";
 }
 
 void step(){
@@ -60,6 +99,13 @@ readFile(){
   FileReader reader = new FileReader();
   reader.onLoadEnd.listen((Event e){postSourceToJsonService(name: fileChoice.files[0].name ,source:reader.result);});
   reader.readAsText(fileChoice.files[0]);
+}
+
+String getJavaSource(String url){
+  String result;
+  HttpRequest.getString(url).then((response) { 
+  print(response);
+  });
 }
 
 postSourceToJsonService({String name, String source}){
