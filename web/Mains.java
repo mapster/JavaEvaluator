ba27b246944f0a454a20c6cc875192e4971a1978
mapster <mapster@ndt-lan.no>
2013-07-11 23:49:36 +0200

    updated test file

--- web/Mains.java
+++ web/Mains.java
@@ -2,7 +2,7 @@ class Mains {
     public static void main(String[] args){
         int x = 5;
         x = 10;
-        x = Mains.funksjon("hei");
+        x = funksjon("hei");
         x = 21;
         if(x == 21){
             x = 22;
